9b265d0ff185dbae217b0e4bdc0ae44413f869b5
Jose-coder-L <156843099+Jose-coder-L@users.noreply.github.com>
2025-08-31 20:59:47 -0400

    Update urls.py

--- crud/urls.py
+++ crud/urls.py
@@ -8,10 +8,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     
     # Secciones de Obesidad
-    path('causes/', views.procedimiento, name='procedimiento'),
+    path('procedimiento/', views.procedimiento, name='procedimiento'),
     path('prevencion/', views.prevention, name='prevención'),
-    path('estadisticas/', views.obesity_statistics, name='statistics'),
-    path('consejos/', views.healthy_tips, name='tips'),
+  
     
     # Eliminar rutas antiguas del CRUD
     # path('tasks/', ...),  # Comenta o elimina estas líneas
